7b90011da16c745d590b796ee7efc44e5ffa0cb7
lhhmmiii <lehuuhung30023010@gmail.com>
2024-07-15 15:18:13 +0700

    feat: add addtional constraints

--- verify_information.py
+++ verify_information.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 import re
 
 def verify_information(miss_item, input_value):
@@ -18,8 +18,33 @@ def verify_information(miss_item, input_value):
         phone_regex = r'^\d{10}$'
         if re.match(phone_regex, input_value):
             return True
+        
+    elif miss_item in ["Ngày sinh", "Tháng sinh", "Năm sinh", "Ngày cấp", "Tháng cấp", "Năm cấp", "Giờ", "Phút", "Giây"]:
+        regex = r'^\d*$'
+        if re.match(regex, input_value):
+            return True
+        
+    elif miss_item == "Ngày tháng năm sinh" or miss_item == "Ngày tháng năm cấp":
+        # Kiểm tra định dạng ngày tháng năm theo dd/mm/yyyy
+        try:
+            datetime.strptime(input_value, '%d/%m/%Y')
+            return True
+        except ValueError:
+            return False
+    
+    elif miss_item == "Chứng minh nhân dân":
+        cccd_regex = r'^\d*$'
+        if re.match(cccd_regex, input_value):
+            return True
+        
+    elif miss_item == "Chỗ ở" or miss_item == "Nơi thường trú":
+        # Kiểm tra chỗ ở có thể chứa chữ cái, số, khoảng trắng và một số ký tự đặc biệt
+        address_regex = r'^[a-zA-Z0-9\s,.-]+$'
+        if re.match(address_regex, input_value):
+            return True
 
     else:
-        return True
+        if input_value and all(x.isalpha() or x.isspace() for x in input_value):
+            return True
 
 
